9958160ebcb88b9dc06bcd0a3f28d9e9277bfbed
Evan Tomczyk <tomczykej@gmail.com>
2019-06-27 09:21:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ function destructivelyRemoveLastKitten(name) {
   kittens.pop(name)
   return kittens;
 }
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens;
+}
